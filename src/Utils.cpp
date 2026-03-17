3fe80f6309bc0b2d555b4df02efad58d18820689
swafle <enzogillin@gmail.com>
2026-03-17 13:13:40 -0400

    this should fix json error

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -57,7 +57,7 @@ int findPairs(vector<int> ids){
             if (aprilTagPairFullJson["AprilTagPairs"][f]["Tag 1"] == ids[u]){
                 for (int c = 0; c < ids.size(); c++){ 
                     if (aprilTagPairFullJson["AprilTagPairs"][f]["Tag 2"] == ids[c]){
-                        pairsPresent.emplace_back(f+1);
+                        pairsPresent.emplace_back(f);
                     }
                 }
             }
