0b3d87689ae6ed89b2f9d4d3764225a79efadfb0
swafle <enzogillin@gmail.com>
2026-03-17 09:42:36 -0400

    fixing main

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,10 @@
 #include <opencv2/core/mat.hpp>
 #include <sstream>
 #include "Utils.h"
-int main(int argc, char* argv[]) {
-    if(argv[1] != nullptr) {
-        switch (argv[1]){
-            case "Utils": {
-                switch (argv[2]) {
-                    case "objPointsOffset": {
-                        cv::Mat oneRow = objPointsOffset(argv[3], argv[4]).reshape(0,1);    // Treat as vector
-                        std::ostringstream os;
-                        os << oneRow;                             // Put to the stream
-                        std::string asStr = os.str();
-                    }
-                }
-            }
-        }
-    } else {
-        std::cout << "Usage: " << std::endl;
-    }
+int main() {
+    cv::Mat oneRow = objPointsOffset(argv[3], argv[4]).reshape(0,1);    // Treat as vector
+    std::ostringstream os;
+    os << oneRow;                             // Put to the stream
+    std::string asStr = os.str();
+    std::cout << asStr << std::endl;
 }
